88278a08ddb90b0456fe85b4b5345fa15d4ebfdd
spiritface57 <abbasi.mahmoudreza@gmail.com>
2025-04-01 15:26:32 -0400

    first init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'no-console': 'error',
+      'no-debugger': 'error',
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      '@typescript-eslint/no-explicit-any': 0,
+      '@typescript-eslint/no-unused-vars': 0,
+    },
+  },
 ];
 
 export default eslintConfig;
